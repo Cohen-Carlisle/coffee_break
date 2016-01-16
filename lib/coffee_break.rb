c72bf82562ce783e83f76ab90fdf89869089973a
Cohen Carlisle <cohen.carlisle@gmail.com>
2016-01-15 22:05:56 -0500

    consolidate class methods

--- lib/coffee_break.rb
+++ lib/coffee_break.rb
@@ -11,4 +11,36 @@ module CoffeeBreak
     Waiter::Coffee.break(timeout: timeout, interval: interval, rescues: rescues,
                          raise: raise).until(message: message, &block)
   end
+
+  def self.default_timeout
+    DEFAULT[:timeout]
+  end
+
+  def self.default_timeout=(seconds)
+    DEFAULT[:timeout] = seconds
+  end
+
+  def self.default_interval
+    DEFAULT[:interval]
+  end
+
+  def self.default_interval=(seconds)
+    DEFAULT[:interval] = seconds
+  end
+
+  def self.default_rescues
+    DEFAULT[:rescues]
+  end
+
+  def self.default_rescues=(exceptions)
+    DEFAULT[:rescues] = exceptions
+  end
+
+  def self.default_raise
+    DEFAULT[:raise]
+  end
+
+  def self.default_raise=(boolean)
+    DEFAULT[:raise] = boolean
+  end
 end
--- lib/coffee_break/defaults.rb
+++ lib/coffee_break/defaults.rb
@@ -1,38 +1,5 @@
 module CoffeeBreak
-  def self.default_timeout
-    DEFAULT[:timeout]
-  end
-
-  def self.default_timeout=(seconds)
-    DEFAULT[:timeout] = seconds
-  end
-
-  def self.default_interval
-    DEFAULT[:interval]
-  end
-
-  def self.default_interval=(seconds)
-    DEFAULT[:interval] = seconds
-  end
-
-  def self.default_rescues
-    DEFAULT[:rescues]
-  end
-
-  def self.default_rescues=(exceptions)
-    DEFAULT[:rescues] = exceptions
-  end
-
-  def self.default_raise
-    DEFAULT[:raise]
-  end
-
-  def self.default_raise=(boolean)
-    DEFAULT[:raise] = boolean
-  end
-
   # private
-
   DEFAULT = {
     timeout: 30,
     interval: 0.1,
